1c749ce14d8ddb6263cc3fb8d7ad9782fe498dac
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-09-17 19:19:51 -0300

    Created Images schema. Created Images NestJS resource, created Module (Controller & Service pending)

---
+++ src/modules/images/images.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ImagesController } from './images.controller';
+import { ImagesService } from './images.service';
+import { Image, ImageSchema } from './schemas/images.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: Image.name, schema: ImageSchema }])],
+  controllers: [ImagesController],
+  providers: [ImagesService],
+})
+export class ImagesModule {}
---
+++ src/modules/images/schemas/images.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ImageDocument = Image & Document;
+
+@Schema()
+export class Image {
+  @Prop({ required: true })
+  url: string;
+
+  @Prop({ required: true })
+  category: string;
+
+  @Prop({ required: true })
+  alt: string;
+
+  @Prop({ required: true })
+  width: number;
+
+  @Prop({ required: true })
+  height: number;
+}
+
+export const ImageSchema = SchemaFactory.createForClass(Image);
